62b3e2f96621a82aa326ff1489d6af964247eaa3
anil <anilkumar2000jnv@gmail.com>
2024-01-24 17:46:26 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 // import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Navbar from './Navbar';
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Navbar from "./Navbar";
 
-
-
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    
-    <Navbar websiteName="ENABLE-ASSIGNMENT"/>
-    <App/>
-   
-
-  
-    
+    <Navbar websiteName="To-Do List" />
+    <App />
   </React.StrictMode>
 );
 
